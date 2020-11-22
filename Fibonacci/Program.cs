0de10b89d67335f7cfdd9fc69f6fe2f885278c37
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-11-22 16:46:57 +0200

    Initial commit

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Zsombor
+ * Date: 2020-11-22
+ * Time: 16:22
+ */
+using System;
+
+namespace Fibonacci
+{
+	class Program
+	{
+		int n = [6, 19, 28, 36, 38];
+		
+		public static void Main(string[] args)
+		{
+			// F_n = Phi^n / sqrt(5) generally rounded up.
+
+			for (var i = 0; i < n.length; i++)
+			{
+				Console.WriteLine(Fibonacci(n[i]));
+			}
+		}
+		
+		private int Fibonacci(int n) {
+    		let p = (1 + Math.sqrt(5))/2;
+    		let a = Math.pow(p, n) / Math.sqrt(5);
+
+   			return Math.round(a);
+		}
+	}
+}
