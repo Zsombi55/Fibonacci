7a06e6a9dfbd91d3968a80c9e54b88ad61309b1d
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-11-22 20:35:05 +0200

    Solved without having to make everything static.
    Really convoluted to have to make an inscance of a class within itself to use its methods within itself. It makes no sense.

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -12,17 +12,18 @@ namespace Fibonacci
 	{
 		public static void Main(string[] args)
 		{
+			Program P = new Program();
 			// F_n = Phi^n / sqrt(5) generally rounded up.
 			double[] n = {6, 19, 28, 36, 38};
 			
 			for (int i = 0; i < n.Length; i++)
 			{ // I don't know why it gives an error, in JS this worked just fine without making the called method static too.
-				Console.WriteLine(Fibonacci(n[i]));
+				Console.WriteLine(P.Fibonacci(n[i]));
 			}
 			Console.ReadLine();
 		}
 		
-		private static double Fibonacci(double n) {
+		private double Fibonacci(double n) {
     		double p = (1 + Math.Sqrt(5))/2;
     		double a = Math.Pow(p, n) / Math.Sqrt(5);
 
