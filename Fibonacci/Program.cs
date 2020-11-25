d94495a81a13b448bdb320eedd2022ed2a6c54ef
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-11-25 17:41:29 +0200

    Adding input from file.
    Began finding a way to get input data from a text file. Don't yet know how to parse & use the data taken.

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -2,9 +2,10 @@
  * Created by SharpDevelop.
  * User: Zsombor
  * Date: 2020-11-22
- * Time: 16:22 - 20:37
+ * Time: 16:22
  */
 using System;
+using System.IO;
 
 namespace Fibonacci
 {
@@ -15,19 +16,42 @@ namespace Fibonacci
 			Program P = new Program();
 			// F_n = Phi^n / sqrt(5) generally rounded up.
 			double[] n = {6, 19, 28, 36, 38};
+			ReadFromFile(@"D:\SULI\C# .NET\Fibonacci\Fibonacci\InputFile.txt");
 			
+			Console.WriteLine();
 			for (int i = 0; i < n.Length; i++)
 			{ // In JS this worked just fine without making the called method static too.
-				Console.WriteLine(P.Fibonacci(n[i]));
+				Console.WriteLine(P.MakeFibonacci(n[i]));
 			}
-			Console.ReadLine();
+									
+			Console.ReadKey();
 		}
 		
-		private double Fibonacci(double n) {
+		private double MakeFibonacci(double n) {
     		double p = (1 + Math.Sqrt(5))/2;
     		double a = Math.Pow(p, n) / Math.Sqrt(5);
 
    			return Math.Round(a);
+		}
+		
+		static void ReadFromFile(string path) {
+			if(File.Exists(path)) {
+				string[] dataFromFile = File.ReadAllLines(path);
+				
+				foreach(string d in dataFromFile) {
+					Console.Write(d + " ");					
+				}
+				Console.WriteLine();
+//				double[] n = new double[dataFromFile.Length];
+//
+//				for(int i = 0; i < dataFromFile.Length; i++)
+//				{
+//					n[i] = Double.Parse(dataFromFile[i]);
+//				}
+			}
+			else {
+				Console.WriteLine("The file doesn't exist or path is wrong.");
+			}
 		}
 	}
 }
