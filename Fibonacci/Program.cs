7e6a0965c376f594465659d9f258446ec9acda82
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-11-27 21:39:38 +0200

    Goal clarification for the misleading name.

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -3,7 +3,14 @@
  * User: Zsombor
  * Date: 2020-11-22
  * This is an exercise from the Catalysts Coding Contest webpage.
- * The goal is not making a Fibonacci sequence as usual, but estimating certain results based on a given series of numbers.
+ * The goal is finding the n-th number of the provided Fibonacci sequence final result(s).
+ * Using the Golden Ratio:  F_n = Phi^n / sqrt(5)  generally rounded up.
+ * 
+ * Excerpt from the test instructions::
+ *  "The Fibonacci sequence  (F_0, F_1, ...)  is due to the recursive law of formation  (F_0 = F_n-1 + F_n-2)
+ * 	for  (n >= 2) with the initial values  (F_0 = 0)  and  (F_1 = 1)  are defined." [...]
+ *  "the first numbers are Zero and One respectively, and each additional number is the sum of their predecessors.
+ *  Examples: n = 0 -> 0 |  n = 1 -> 1 |  n = 2 -> 1 |  n = 3 -> 2 |  n = 4 -> 3 |  n = 5 -> 5 ."
  */
 using System;
 using System.IO;
@@ -14,8 +21,6 @@ namespace Fibonacci
 	{
 		public static void Main(string[] args)
 		{
-			
-			// F_n = Phi^n / sqrt(5) generally rounded up.
 			//long[] n = {6, 19, 28, 36, 38};
 			long[] nr = ReadFromFile(@"D:\SULI\C# .NET\Fibonacci\Fibonacci\InputFile.txt");
 			
@@ -50,7 +55,7 @@ namespace Fibonacci
 			}
 			else {
 				Console.WriteLine("The file doesn't exist or path is wrong.");
-				return new long[0];
+				return new long[0]; // an empty array.
 			}
 		}
 	}
